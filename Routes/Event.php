4cec72c41cf1909ee470f6a8a6908b7532111f1c
daniel-qwerty <daniel@qwerty.com.bo>
2016-10-19 15:33:09 -0400

    asd

--- Routes/Event.php
+++ Routes/Event.php
@@ -2,6 +2,6 @@
 
 class Routes_Event extends Com_Application_Route {
 
-    public $pattern = "/^(\w+|-)+\/event/";
+    public $pattern = "/^(\w+|-)+\/item/";
     public $result = "_0_/Index/Events/Item/";
 }
