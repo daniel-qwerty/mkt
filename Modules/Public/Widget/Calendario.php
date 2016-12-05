90d883857f6cc9c0d79fe430974d5cb50ea66c3b
csuaznabar <carlos.sznbr@gmail.com>
2016-12-05 17:33:38 -0400

    correccion en calendario y en widget de posts

--- Modules/Public/Widget/Calendario.php
+++ Modules/Public/Widget/Calendario.php
@@ -1,6 +1,7 @@
 <?php
 
-class Public_Widget_Calendario extends Com_Object {
+class Public_Widget_Calendario extends Com_Object
+{
 
     public $lan;
 
@@ -10,16 +11,19 @@ class Public_Widget_Calendario extends Com_Object {
      * @access public
      * @return Public_Widget_Calendario
      */
-    public static function getInstance() {
+    public static function getInstance()
+    {
         return self::_getInstance(__CLASS__);
     }
 
-    public function setLan($lan) {
+    public function setLan($lan)
+    {
         $this->lan = $lan;
         return $this;
     }
 
-    public function render() {
+    public function render()
+    {
         ?>
         <div class="sidebar-module bg-gray text-left">
             <h5>Calendar</h5>
@@ -36,6 +40,28 @@ class Public_Widget_Calendario extends Com_Object {
                         <i class="fa fa-arrow-left"
                            style="font-size: 16px;display: inline-block;text-align: center;vertical-align: middle;transition: .4s;"></i>
                     </a>
+                </div>
+                <div class="rdc-events">
+
+                    <ul>
+                        ////////////////////// iterar los ultimos eventos //////////////////////////////
+                        <li class="rdc-event" data-date="12/28/2016">
+                            <a href="http://neblux.com">Lorem ipsum dolor it asdasd sdfdsfjkhkj asd</a>
+                        </li>
+                        <li class="rdc-event" data-date="12/22/2016">
+                            Event 2
+                        </li>
+                        <li class="rdc-event" data-date="12/29/2016">
+                            Event 2
+                        </li>
+                        <li class="rdc-event" data-date="12/15/2016">
+                            Event 2
+                        </li>
+                        <li class="rdc-event" data-date="12/29/2016">
+                            Event 2
+                        </li>
+                        ////////////////////// iterar los ultimos eventos //////////////////////////////
+                    </ul>
                 </div>
                 <div class="rdc-table"></div>
             </div>
