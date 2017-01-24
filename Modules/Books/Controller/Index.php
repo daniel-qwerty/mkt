250cf62f780a8386a41039a1a20d2909fd7e71b3
System Administrator <root@MacBook-Air-de-Skynet.local>
2017-01-24 14:51:51 -0400

    algunos bugs corregidos

--- Modules/Books/Controller/Index.php
+++ Modules/Books/Controller/Index.php
@@ -29,25 +29,31 @@ class Books_Controller_Index extends Public_Controller_Index {
 
         $images = Books_Model_Media::getInstance()->getListByProject($url, $this->lan->LanId);
         $this->assign("images", $images);
-
         $client = get('sessionCliente');
-        $this->assign("client", $client);     
-        
+        //echo 'xxxxx-> '.$client;
+        if (!empty($client)) {
 
-        if (get('cliente')!= 0) {
-            $carrito = Clients_Model_Venta::getInstance()->getListCarrito(date('Y-m-d'), $this->client->CliId);
-            $this->assign("carrito", $carrito);
+            $this->assign("client", $client);
 
-            $Sumcarrito = Clients_Model_Venta::getInstance()->getSumCarrito(date('Y-m-d'), $this->client->CliId);
-            $this->assign("Sumcarrito", $Sumcarrito[0]->total);
-            
-            $venta = Clients_Model_Venta::getInstance()->getVenta($this->client->CliId, date('Y-m-d'));
-            $this->assign("VentId", $venta->VenId);
-        }  else {
-            $this->assign("carrito", []);
-            $this->assign("Sumcarrito", '0');
-            $this->assign("VentId", '0');
-        }
+            if (!empty(get('cliente'))) {
+                $carrito = Clients_Model_Venta::getInstance()->getListCarrito(date('Y-m-d'), $this->client->CliId);
+                $this->assign("carrito", $carrito);
+
+                $Sumcarrito = Clients_Model_Venta::getInstance()->getSumCarrito(date('Y-m-d'), $this->client->CliId);
+                $this->assign("Sumcarrito", $Sumcarrito[0]->total);
+
+                $venta = Clients_Model_Venta::getInstance()->getVenta($this->client->CliId, date('Y-m-d'));
+                $this->assign("VentId", $venta->VenId);
+            } else {
+                $this->assign("carrito", []);
+                $this->assign("Sumcarrito", '0');
+                $this->assign("VentId", '0');
+            }
+        }else {
+                $this->assign("carrito", []);
+                $this->assign("Sumcarrito", '0');
+                $this->assign("VentId", '0');
+            }
     }
 
 }
--- Modules/Suscription/Controller/Index.php
+++ Modules/Suscription/Controller/Index.php
@@ -7,29 +7,34 @@ class Suscription_Controller_Index extends Public_Controller_Index {
         $this->setView("list");
         $url = get('REQUEST_URI');
         $url = explode("/", $url);
-        $url = $url[count($url) - 1];  
-        
+        $url = $url[count($url) - 1];
+
         $list = Suscription_Model_Suscription::getInstance()->getList2($this->lan->LanId);
         $this->assign("list", $list);
-        
-        $client = get('sessionCliente');
-        $this->assign("client", $client);         
-        
-        if (get('cliente')!= 0) {
-            $carrito = Clients_Model_Venta::getInstance()->getListCarrito(date('Y-m-d'), $this->client->CliId);
-            $this->assign("carrito", $carrito);
 
-            $Sumcarrito = Clients_Model_Venta::getInstance()->getSumCarrito(date('Y-m-d'), $this->client->CliId);
-            $this->assign("Sumcarrito", $Sumcarrito[0]->total);
-            
-            $venta = Clients_Model_Venta::getInstance()->getVenta($this->client->CliId, date('Y-m-d'));
-            $this->assign("VentId", $venta->VenId);
-        }  else {
-            $this->assign("carrito", []);
-            $this->assign("Sumcarrito", '0');
-            $this->assign("VentId", '0');
-        }
-       
+        $client = get('sessionCliente');
+        if (!empty($client)) {
+            $this->assign("client", $client);
+
+            if (get('cliente') != 0) {
+                $carrito = Clients_Model_Venta::getInstance()->getListCarrito(date('Y-m-d'), $this->client->CliId);
+                $this->assign("carrito", $carrito);
+
+                $Sumcarrito = Clients_Model_Venta::getInstance()->getSumCarrito(date('Y-m-d'), $this->client->CliId);
+                $this->assign("Sumcarrito", $Sumcarrito[0]->total);
+
+                $venta = Clients_Model_Venta::getInstance()->getVenta($this->client->CliId, date('Y-m-d'));
+                $this->assign("VentId", $venta->VenId);
+            } else {
+                $this->assign("carrito", []);
+                $this->assign("Sumcarrito", '0');
+                $this->assign("VentId", '0');
+            }
+        }else {
+                $this->assign("carrito", []);
+                $this->assign("Sumcarrito", '0');
+                $this->assign("VentId", '0');
+            }
     }
 
 }
